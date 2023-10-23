f368abf9c909f718b751baa50d5db5d09bfa5798
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-23 17:35:33 +0700

    Add greeting endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # get 'root/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'root#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get 'greeting/random', to: 'greeting#random_greeting'
+    end
+  end
 end
