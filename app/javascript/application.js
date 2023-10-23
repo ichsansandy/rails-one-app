4d1a194c9d958e37ebbf4d0021979137a4976392
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-23 17:24:00 +0700

    Add react router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,46 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyComponent from "./components/MyComponent";
+import Home from './components/Home';
+import Greeting from './components/Greeting';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <MyComponent />
+    <div className="App">
+      <nav className="navbar">
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active-link' : 'default-link')}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active-link' : 'default-link')}
+          to="/greetings"
+        >
+          Greetings
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="greetings"
+          element={<Greeting />}
+        />
+      </Routes>
     </div>
-    );
+  );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
--- app/javascript/components/MyComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MyComponent() {
-  return (
-    <div>MyComponent</div>
-  )
-}
